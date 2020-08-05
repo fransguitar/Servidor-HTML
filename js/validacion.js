0d6bbd042aed60d8f90332db2ac4f031413e81b3
Frans guillermo <fransguitar@gmail.com>
2020-08-05 18:38:46 -0500

    registro de usuario

--- js/validacion.js
+++ js/validacion.js
@@ -1,3 +1,19 @@
 $(document).ready(function () {
-    
+    var brithdate= $("#date").val();
+    var hoy = new Date();
+    var dd=0;
+    var m;
+    $("#edad").click(function (e) { 
+        e.preventDefault();
+        var hoyy= hoy.getFullYear();
+        brithdate= new Date(brithdate);
+        var brithdatey= brithdate.getFullYear();
+        dd=hoyy-brithdatey;
+        m = hoy.getMonth() - brithdate.getMonth();
+        if (m < 0 || (m === 0 && hoy.getDate() < brithdate.getDate())) {
+            dd--;
+        }
+        $("#edad").val(dd);
+        
+    });
 });
